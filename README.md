af3b6fbf01033eead44338458de082054260e07f
axel.paris@ynov.com <axel.paris@ynov.com>
2021-01-08 19:33:52 +0100

    add findOne to get one course details

--- README.md
+++ README.md
@@ -40,6 +40,31 @@ discount_urgency (Prix entre MIN et MAX pendant 31 jours)
 urgency_discount (Prix MIN pendant 5 jours)
 ```
 
+## Functions
+
+### udemy.coupons.create(courseId, couponCode, value, discountStrategy)
+
+| Paramètre          | Description                 | Valeur                                                                                   |
+| ------------------ | --------------------------- | ---------------------------------------------------------------------------------------- |
+| `courseId`         | Udemy course ID             | Ex: `2393094`                                                                            |
+| `couponCode`       | Coupon                      | Ex: `NOUVELAN2020`                                                                       |
+| `value`            | Price (between 9.99 et 199) | Ex: `13.99`                                                                              |
+| `discountStrategy` | Type de promotion           | - `scarcity_free`<br/>- `urgency_free`<br/>- `discount_urgency`<br/>- `urgency_discount` |
+
+### udem.announcements.send(title, content, includes = [], excludes = [], completionRatio = [0, 49, 99, 100], fromDate = '', toDate = '', is_promotional = true, isPreview = true)
+
+| Paramètre         | Description                              | Valeur                         |
+| ----------------- | ---------------------------------------- | ------------------------------ |
+| `title`           | Title                                    | Ex: `Nouveau cours : Adobe XD` |
+| `content`         | HTML content                             | Ex: `<p>Inscris-toi !</p>`     |
+| `includes`        | Tableau des ID des cours Udemy à cibler  | Ex: `[2393094, 2872087]`       |
+| `excludes`        | Tableau des ID des cours Udemy à exclure | Ex: `[2393094, 2872087]`       |
+| `completionRatio` | Filtre de progression des participants   | -                              |
+| `fromDate`        | Filtre de date de début d'inscription    | -                              |
+| `toDate`          | Filtre de date de fin d'inscription      | -                              |
+| `is_promotional`  | Is promotional                           | `true` or `false`              |
+| `isPreview`       | Is preview email                         | `true` or `false`              |
+
 ## Information
 
 This package is not under current development but you can DM me ou open pull request if you want to participate.
--- src/resources/courses.js
+++ src/resources/courses.js
@@ -4,12 +4,10 @@ class Courses {
     this.axios = axios
   }
 
-  async get(courseId = null) {
-    const response = await this.axios.get(
-      `https://www.udemy.com/api-2.0/users/me/taught-courses/?page=1&page_size=200&ordering=-created&skip_caching=true&fields[course]=title,image_100x100,image_200_H,status_label,is_paid,is_private,original_price_text,privacy_label,quality_status,can_edit,can_use_course_manage,url,creation_progress_ratio,is_published,num_subscribers,num_subscribers_recent,features,rating,quality_feedback_counts,quality_review_process,can_use_revenue_report,can_use_qa,can_use_reviews,is_owner,is_organization_eligible,show_organization_eligible_reminder,is_in_any_ufb_content_collection,content_length_video&fields[feature]=reviews_view&fields[quality_review_process]=last_submitted_date`
-    )
+  async get() {
+    const response = await this.axios.get(`https://www.udemy.com/api-2.0/users/me/taught-courses/?page=1&page_size=200&ordering=-created&skip_caching=true&fields[course]=title,image_100x100,image_200_H,status_label,is_paid,is_private,original_price_text,privacy_label,quality_status,can_edit,can_use_course_manage,url,creation_progress_ratio,is_published,num_subscribers,num_subscribers_recent,features,rating,quality_feedback_counts,quality_review_process,can_use_revenue_report,can_use_qa,can_use_reviews,is_owner,is_organization_eligible,show_organization_eligible_reminder,is_in_any_ufb_content_collection,content_length_video&fields[feature]=reviews_view&fields[quality_review_process]=last_submitted_date`)
 
-    const courses = response.data.results.map((course) => {
+    courses = response.data.results.map((course) => {
       if (course._class === 'course') {
         return {
           ...course,
@@ -19,6 +17,15 @@ class Courses {
     })
 
     return courses
+  }
+
+  async getOne(courseId) {
+    const response = await this.axios.get(`https://www.udemy.com/api-2.0/courses/${courseId}/?fields[course]=title,image_100x100,image_200_H,status_label,is_paid,is_private,original_price_text,privacy_label,quality_status,can_edit,can_use_course_manage,url,creation_progress_ratio,is_published,num_subscribers,num_subscribers_recent,features,rating,quality_feedback_counts,quality_review_process,can_use_revenue_report,can_use_qa,can_use_reviews,is_owner,is_organization_eligible,show_organization_eligible_reminder,is_in_any_ufb_content_collection,content_length_video&fields[feature]=reviews_view&fields[quality_review_process]=last_submitted_date`)
+
+    return {
+      ...response.data,
+      url: `https://udemy.com${response.data.url}`,
+    }
   }
 }
 
