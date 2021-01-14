f9d9ae776acae7fc413d21db915aa02a61ccf80c
axel.paris@ynov.com <axel.paris@ynov.com>
2021-01-14 22:24:46 +0100

    rm yarn lock

--- src/resources/courses.js
+++ src/resources/courses.js
@@ -7,7 +7,7 @@ class Courses {
   async get() {
     const response = await this.axios.get(`https://www.udemy.com/api-2.0/users/me/taught-courses/?page=1&page_size=200&ordering=-created&skip_caching=true&fields[course]=title,image_100x100,image_200_H,status_label,is_paid,is_private,original_price_text,privacy_label,quality_status,can_edit,can_use_course_manage,url,creation_progress_ratio,is_published,num_subscribers,num_subscribers_recent,features,rating,quality_feedback_counts,quality_review_process,can_use_revenue_report,can_use_qa,can_use_reviews,is_owner,is_organization_eligible,show_organization_eligible_reminder,is_in_any_ufb_content_collection,content_length_video&fields[feature]=reviews_view&fields[quality_review_process]=last_submitted_date`)
 
-    courses = response.data.results.map((course) => {
+    return response.data.results.map((course) => {
       if (course._class === 'course') {
         return {
           ...course,
@@ -15,8 +15,6 @@ class Courses {
         }
       }
     })
-
-    return courses
   }
 
   async getOne(courseId) {
